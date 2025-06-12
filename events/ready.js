a533da6dfa728d704653efe4d7f8790e16fc6eb5
Tejas837373737 <126201452+Tejas837373737@users.noreply.github.com>
2025-06-12 22:47:26 +0530

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(client) {
         console.log(` Ready! Logged in as ${client.user.tag} ✅✅✅`); // Added emojis to make it stand out
         // Your presence/activity code would also be here
-        client.user.setActivity('shivang heart');
+        client.user.setActivity('/help');
         console.log('Giveaway checker started.');
         startGiveawayChecker(client);
     },
